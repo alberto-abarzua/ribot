83d2d0bdbb0cf449a4bedbae92cbf0f656ada394
Alberto Abarzua <aabarzua@palvi.cl>
2022-05-12 22:05:07 -0400

    nothing works

--- python_code/arm_utils/serial_monitor.py
+++ python_code/arm_utils/serial_monitor.py
@@ -3,6 +3,7 @@ import serial
 import sys
 import os
 from collections import defaultdict
+import time
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from arm_control.status import *
@@ -18,13 +19,14 @@ class Monitor():
         self.sta_dict["m"] = READY_STATUS
         self.sta_dict["i"] = PRINTED
 
+
         arduino = serial.Serial(baudrate = 115200,port =port,timeout=5)
         arduino.close()
         arduino.open()
         self.arduino = arduino
         if (self.arduino.read(1).decode() == INITIALIZED):
             self.ini = True
-        print("Arduino is initialized!")
+            print("Arduino is initialized!")
 
     def my_run(self,m):
         m = m.encode()
@@ -34,13 +36,25 @@ class Monitor():
         if (status == PRINTED):
             res = self.arduino.read_until(PRINTED.encode())
         else:
-            res = self.arduino.read(1)
-
+            return
         try:
             res = res.decode()
             print(res)
         except:
             print("Value that generated the error {}".format(res))
+
+    def interp(self,mes):
+        mes = mes.split(" ")
+        op = mes[0][0]
+        if(op == ":"):
+            op = mes[0][1]
+            mes[0] = mes[0][1:]
+            loop = True
+        code = int(mes[0][1:])
+        args = mes[1:]
+        args = [int(x) for x in args]
+        m = Message(op,code,args)
+        return m,op
 
     def run(self):
         while(True):
@@ -49,16 +63,7 @@ class Monitor():
             loop = False
             
             try:
-                mes = mes.split(" ")
-                op = mes[0][0]
-                if(op == ":"):
-                    op = mes[0][1]
-                    mes[0] = mes[0][1:]
-                    loop = True
-                code = int(mes[0][1:])
-                args = mes[1:]
-                args = [int(x) for x in args]
-                m = Message(op,code,args)
+               m,op = self.interp(mes)
             except:
                 print("Please enter a valid input.")
                 continue;
@@ -72,8 +77,47 @@ class Monitor():
 
 
 if __name__ == "__main__":
-    if (len(sys.argv)>1):
+   
+    if (len(sys.argv)== 2):
         PORT = sys.argv[1]
-    m = Monitor()
-    m.run()
+        print("Starting on port {}".format(PORT))
+        m = Monitor()
+        m.run()
+    elif (len(sys.argv) >=3):
+        PORT = sys.argv[1]
+        file = sys.argv[2]
+        m = Monitor()
+        times = 1
+        if (len(sys.argv) == 4):
+            times = int(sys.argv[3])
+        with open(file="scripts/" + file) as f:
+            lines =  f.readlines()
+            for _ in range(times):
+                n=0
+                for line in lines:
+                    mes,op = m.interp(line)
+                    m.my_run(mes)
+                    if m.arduino.in_waiting >=1:
+                        res =m.arduino.read(1).decode()
+                        if (int(res) == 2):
+                            print("Ran {} commands before receiving BUSY".format(n))
+                            m.my_run(Message("i",4))
+
+                            break
+                    n+=1
+            time.sleep(4)
+            m.my_run(Message("i",4))
+            s = m.arduino.read_all()
+            print(s.decode())
     
+    
+
+"""_summary_
+
+m1 7853 0 0 0 0 0
+i1
+m1 -7853 0 0 -0 -0 -0
+:i1
+
+
+"""
