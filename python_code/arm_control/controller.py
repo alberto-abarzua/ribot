9a38da2bd636a59ad1e2706afc0d92efff9f4028
Alberto Abarzua <aabarzua@palvi.cl>
2022-05-24 15:49:22 -0400

    fix chmod for mac

--- python_code/arm_control/controller.py
+++ python_code/arm_control/controller.py
@@ -298,8 +298,8 @@ class Controller():
                 self.proc_sim  = subprocess.Popen(os.path.abspath("../arm_sim_app/arm_sim.exe")) #Starts the simulation.
             else:
                 path = os.path.abspath("../arm_sim_app_mac.app")
-                os.chmod(path,-stat.S_IREAD)
-                os.chmod(path,stat.S_IXUSR )
+                os.chmod(path,~stat.S_IREAD  & stat.S_IXUSR )
+                os.chmod(path,)
                 self.proc_sim = subprocess.Popen(path) #Starts the simulation.
         self.monitor.run()
 
