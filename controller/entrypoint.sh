aacbd249cd9c27994833c36ac9e139e4ec2a80a3
alberto-abarzua <uintuser@pm.me>
2023-07-02 18:43:22 -0400

    fix: linting issues

--- controller/entrypoint.sh
+++ controller/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # if $TEST is set, run tests
-echo "TEST: $TEST"
 if [ "$TEST" = "true" ]; then
     echo "Running tests"
     pdm run test
--- controller/src/tests/test_controller.py
+++ controller/src/tests/test_controller.py
@@ -1,5 +1,3 @@
-import os
-import socket
 import time
 import unittest
 
