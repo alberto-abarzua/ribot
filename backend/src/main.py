219b8258aba5e5fed022c8bed61dc64116c7d8a9
alberto-abarzua <uintuser@pm.me>
2023-08-24 21:09:47 -0400

    chore: update dir names

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,4 @@
-from robot_arm_controller.control.controller import ArmController, Settings
+from robot_arm_controller.controller import ArmController, Settings
 
 
 if __name__ == "__main__":
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     if healthy:
         controller.home()
         controller.set_setting_joints(Settings.SPEED_RAD_PER_S, 0.2)
-
         while True:
             zero = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
             thirty = [30 * 3.14 / 180 for _ in range(6)]
