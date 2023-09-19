7dcdcb0724e8014a77532b45cbf5121c11f44110
alberto-abarzua <uintuser@pm.me>
2023-09-19 14:20:59 -0300

    fix(servo.cpp): move defines

--- firmware/components/movement/servo.cpp
+++ firmware/components/movement/servo.cpp
@@ -1,11 +1,5 @@
 #include "movement.h"
 
-#ifdef ESP_PLATFORM
-
-static const char *TAG = "example";
-
-// Please consult the datasheet of your servo before changing the following
-// parameters
 #define SERVO_MIN_PULSEWIDTH_US 500   // Minimum pulse width in microsecond
 #define SERVO_MAX_PULSEWIDTH_US 2500  // Maximum pulse width in microsecond
 #define SERVO_MIN_DEGREE -90          // Minimum angle
@@ -13,6 +7,13 @@ static const char *TAG = "example";
 
 #define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
 #define SERVO_TIMEBASE_PERIOD 20000           // 20000 ticks, 20ms
+
+#ifdef ESP_PLATFORM
+
+static const char *TAG = "SERVO_SETUP";
+
+// Please consult the datasheet of your servo before changing the following
+// parameters
 
 void Servo::hardware_setup() {
     ESP_LOGI(TAG, "Create timer and operator");
