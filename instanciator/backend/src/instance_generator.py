6f8b29c3baa499b2937347399adf9ee0db0ba068
alberto-abarzua <uintuser@pm.me>
2023-11-18 17:43:36 -0300

    chore: start refactor of instanciator

--- instanciator/backend/src/instance_generator.py
+++ instanciator/backend/src/instance_generator.py
@@ -7,8 +7,10 @@ import time
 import uuid
 from pathlib import Path
 from typing import Any, Dict, Optional
-
+import dataclasses
 import redis
+import pickle
+from threading import Lock
 
 PARENT_FILE_PATH = Path(__file__).parent
 
@@ -18,184 +20,91 @@ DOCKER_COMPOSE_FILE_PATH = (
 )
 
 
+@dataclasses.dataclass
+class Ports:
+    backend_http_port: int
+    controller_websocket_port: int
+    controller_server_port: int
 
-class InstanceGenerator:
-    def __init__(self) -> None:
-        self.docker_compose_path = str(DOCKER_COMPOSE_FILE_PATH)
-        self.redis_client = redis.Redis(
-            host="localhost", port=6379, db=0, decode_responses=True
-        )
-        self.healty_timeout = 60 * 3
-        self.old_instance_timeout = 30 * 60
-        self.min_instances = 5
-        self.prune_interval = 60 * 20
-
-        self.start_instance_checker()
-
-    def check_instance_health(self, uuid_str: str) -> bool:
-        try:
-            project_name = self.get_project_name(uuid_str)
-            instances = self.instances
-            instance = instances[uuid_str]
-
-            env_vars = {
-                "BACKEND_HTTP_PORT": str(instance["ports"]["backend_http_port"]),
-                "CONTROLLER_WEBSOCKET_PORT": str(instance["ports"]["controller_websocket_port"]),
-                "CONTROLLER_SERVER_PORT": str(instance["ports"]["controller_server_port"]),
-                "ESP_CONTROLLER_SERVER_PORT": str(instance["ports"]["controller_server_port"]),
-            }
-
-            command = ["docker", "compose", "-f", self.docker_compose_path,
-                       '-p', project_name, "ps", "--format", "json"]
-            result = subprocess.check_output(command, env={**os.environ, **env_vars})
-
-            services = result.decode("utf-8").strip().split('\n')
-
-            for service_str in services:
-                service = json.loads(service_str)
-                if service["State"] != "running":
-                    return False
-            return True
-        except Exception as e:
-
-            print("health check failed")
-            print("uuid_str", uuid_str)
-            print(e)
-
-
-            return False
-
-    def instance_checker_target_fun(self) -> None:
-        last_prune = 0
-
-        while True:
-            instances = self.instances
-            free_instances = 0
-
-            for instance in instances.values():
-                instance_uuid = instance["uuid"]
-                time_started = instance["time_started"]
-                healthy = self.check_instance_health(instance_uuid)
-                last_health_check = instance["last_health_check"]
-
-                is_old = time.time() - time_started > self.old_instance_timeout
-                old_health_check = last_health_check is not None and last_health_check != 0 and time.time() - \
-                    last_health_check > self.healty_timeout
-
-                if is_old or old_health_check or not healthy:
-                    print('\n------------------')
-                    print("\tdestroying", instance_uuid)
-                    print('\tis_old', is_old)
-                    print('\told_health_check', old_health_check)
-                    print('\thealthy', healthy)
-                    print('\n------------------')
-
-                    self.destroy(instance_uuid)
-                    continue
-
-                if instance["free"]:
-                    free_instances += 1
-
-
-            if free_instances <= self.min_instances:
-                new_uuid = str(uuid.uuid4())
-
-                self.instances = instances
-                new_instance = self.create_instance(new_uuid)
-                instances = self.instances
-
-                new_instance["free"] = True
-                instances[new_uuid] = new_instance
-
-            self.instances = instances
-
-            if time.time() - last_prune > self.prune_interval:
-                self.prune()
-                last_prune = time.time()
-
-
-
-    def prune(self) -> None:
-        command = ["docker", "system", "prune", "-f"]
-        try:
-            subprocess.check_call(command)
-        except subprocess.CalledProcessError:
-            pass
-
-    def start_instance_checker(self) -> None:
-        thread = threading.Thread(target=self.instance_checker_target_fun)
-        thread.start()
-
-    def get_instances(self) -> Dict[str, Any]:
-        instances = self.instances
-        for instance in instances.values():
-            instance["healthy"] = self.check_instance_health(instance["uuid"])
-        return instances
-
-    @property
-    def instances(self) -> Dict[str, Any]:
-        json_str = self.redis_client.get("instances")
-        if json_str is None:
-            return {}
-        else:
-            return json.loads(str(json_str))
-
-    @instances.setter
-    def instances(self, value: Dict[str, Any]) -> None:
-        self.redis_client.set("instances", json.dumps(value))
-
-    def get_instance(self, uuid_str: str) -> Optional[Dict[str, Any]]:
-        if uuid_str in self.instances:
-            return self.instances[uuid_str]
-        return None
-
-    def get_free_port(self) -> int:
-        while True:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.bind(("", 0))  
-                s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                port = s.getsockname()[1]
-                return port
-
-    def get_project_name(self, uuid_str: str) -> str:
-        return f"rtwin_instance_{uuid_str}"
-
-    def get_uuid(self) -> str:
-        instances = self.instances
-        for instance in instances.values():
-            if instance["free"]:
-                instance["free"] = False
-                self.instances = instances
-                return instance["uuid"]
-
-        new_uuid = str(uuid.uuid4())
-        new_instance = self.create_instance(new_uuid)
-        new_instance["free"] = False
-        new_instance["last_health_check"] = time.time()
-        instances[new_uuid] = new_instance
-        self.instances = instances
-        return new_uuid
-
-
-    def create_instance(self, uuid_str: str) -> Dict[str, Any]:
-        backend_http_port = self.get_free_port()
-        controller_websocket_port = self.get_free_port()
-        controller_server_port = self.get_free_port()
-
-        env_vars = {
-            "BACKEND_HTTP_PORT": str(backend_http_port),
-            "CONTROLLER_WEBSOCKET_PORT": str(controller_websocket_port),
-            "CONTROLLER_SERVER_PORT": str(controller_server_port),
-            "ESP_CONTROLLER_SERVER_PORT": str(controller_server_port),
+    def as_dict(self) -> Dict[str, int]:
+        return {
+            "backend_http_port": self.backend_http_port,
+            "controller_websocket_port": self.controller_websocket_port,
+            "controller_server_port": self.controller_server_port,
         }
 
-        project_name = self.get_project_name(uuid_str)
+
+class Instance:
+    instance_uuid: str
+    time_created: float
+    free: bool
+    ports: Ports
+    time_last_health_check: Optional[float]
+
+    def __init__(self):
+        self.instance_uuid = str(uuid.uuid4())
+        self.time_created = time.time()
+        self.time_old_timout = 60 * 30
+        self.time_old_health_check = 60 * 5
+        self.time_last_health_check = None
+        self.free = True
+        self.ports = self.get_ports()
+
+    def get_free_port(self) -> int:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(("", 0))
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            port = s.getsockname()[1]
+            return port
+
+    def get_ports(self) -> Ports:
+        if self.ports is None:
+            self.ports = Ports(
+                backend_http_port=self.get_free_port(),
+                controller_websocket_port=self.get_free_port(),
+                controller_server_port=self.get_free_port(),
+            )
+        return self.ports
+
+    def get_project_name(self) -> str:
+        return f"rtwin_instance_{self.instance_uuid}"
+
+    def get_env_vars(self) -> Dict[str, str]:
+        return {
+            "BACKEND_HTTP_PORT": str(self.ports.backend_http_port),
+            "CONTROLLER_WEBSOCKET_PORT": str(self.ports.controller_websocket_port),
+            "CONTROLLER_SERVER_PORT": str(self.ports.controller_server_port),
+            "ESP_CONTROLLER_SERVER_PORT": str(self.ports.controller_server_port),
+        }
+
+    def health_check(self) -> bool:
+        project_name = self.get_project_name()
+        env_vars = self.get_env_vars()
+
+        command = ["docker", "compose", "-f", DOCKER_COMPOSE_FILE_PATH,
+                   '-p', project_name, "ps", "--format", "json"]
+        result = subprocess.check_output(command, env={**os.environ, **env_vars})
+
+        services = result.decode("utf-8").strip().split('\n')
+
+        for service_str in services:
+            service = json.loads(service_str)
+            if service["State"] != "running":
+                return False
+        return True
+
+    def set_last_health_check(self) -> None:
+        self.time_last_health_check = time.time()
+
+    def start(self) -> None:
+        env_vars = self.get_env_vars()
+        project_name = self.get_project_name()
 
         command = [
             "docker",
             "compose",
             "-f",
-            self.docker_compose_path,
+            DOCKER_COMPOSE_FILE_PATH,
             "-p",
             project_name,
             "up",
@@ -206,72 +115,86 @@ class InstanceGenerator:
         if result != 0:
             raise Exception("docker compose failed")
 
-        return {
-            "ports": {
-                "backend_http_port": backend_http_port,
-                "controller_websocket_port": controller_websocket_port,
-                "controller_server_port": controller_server_port,
-            },
-            "time_started": time.time(),
-            "free": True,
-            "uuid": uuid_str,
-            "last_health_check": None,
+    def stop(self) -> None:
+        env_vars = self.get_env_vars()
+        project_name = self.get_project_name()
 
+        command = [
+            "docker",
+            "compose",
+            "-f",
+            DOCKER_COMPOSE_FILE_PATH,
+            "-p",
+            project_name,
+            "down",
+            "--remove-orphans",
+        ]
+
+        result = subprocess.check_call(command, env={**os.environ, **env_vars})
+        if result != 0:
+            raise Exception("docker compose failed")
+
+    def should_stop(self) -> bool:
+        old = time.time() - self.time_created > 30 * 60
+        healthy = self.health_check()
+        old_health_check = self.time_last_health_check is not None and time.time(
+        ) - self.time_last_health_check > 60 * 5
+        return old or old_health_check or not healthy
+
+    def as_dict(self) -> Dict[str, Any]:
+        return {
+            "instance_uuid": self.instance_uuid,
+            "time_created": self.time_created,
+            "free": self.free,
+            "ports": self.ports,
+            "time_last_health_check": self.time_last_health_check,
+            "healthy": self.health_check(),
         }
 
-    def set_last_health_check(self, uuid_str: str) -> None:
-        instances = self.instances
-        if uuid_str in instances:
-            print("\thealth check", uuid_str)
-            instances[uuid_str]["last_health_check"] = time.time()
-            self.instances = instances
 
-    def destroy(self, uuid_str: str) -> None:
-        instances = self.instances
-        if uuid_str in instances:
-            print("\tdestroying", uuid_str)
-            project_name = self.get_project_name(uuid_str)
-            instance = instances[uuid_str]
+class InstanceGenerator:
+    def __init__(self) -> None:
+        self.docker_compose_path = str(DOCKER_COMPOSE_FILE_PATH)
+        self.redis_client = redis.Redis(
+            host="localhost", port=6379, db=0
+        )
 
-            env_vars = {
-                "BACKEND_HTTP_PORT": str(instance["ports"]["backend_http_port"]),
-                "CONTROLLER_WEBSOCKET_PORT": str(
-                    instance["ports"]["controller_websocket_port"]
-                ),
-                "CONTROLLER_SERVER_PORT": str(
-                    instance["ports"]["controller_server_port"]
-                ),
-                "ESP_CONTROLLER_SERVER_PORT": str(
-                    instance["ports"]["controller_server_port"]
-                ),
-            }
-            command = [
-                "docker",
-                "compose",
-                "-f",
-                self.docker_compose_path,
-                "-p",
-                project_name,
-                "down",
-                "--remove-orphans",
-            ]
-            print(command)
-            result = subprocess.check_call(command, env={**os.environ, **env_vars})
-            if result != 0:
-                raise Exception("docker ", "compose failed")
-            instances = self.instances
-            del instances[uuid_str]
-            self.instances = instances
+        self.instances_lock = Lock()
+        self.start_instance_checker()
 
-    def destroy_all(self) -> None:
-        instances = self.instances
-        for uuid_str in instances:
-            self.destroy(uuid_str)
+    def instance_checker_target_fun(self) -> None:
+        pass
 
-    def get_backend_port(self, uuid_str: str) -> Optional[int]:
-        instances = self.instances
-        port = None
-        if uuid_str in instances:
-            port = instances[uuid_str]["ports"]["backend_http_port"]
+    @staticmethod
+    def redis_instances(func):
+        def wrapper(self, *args, **kwargs):
+            try:
+                with self.instances_lock:
+                    redis_object = self.redis_client.get("instances")
+                    if redis_object is not None:
+                        self.instances = pickle.loads(redis_object)
+                    else:
+                        self.instances = []
 
-        return port
+                    result = func(*args, **kwargs)
+
+                    pickled_instances = pickle.dumps(self.instances)
+                    self.redis_client.set("instances", pickled_instances)
+                    return result
+
+            except (redis.RedisError, pickle.PickleError) as error:
+                print(f"An error occurred: {error}")
+                raise
+        return wrapper
+
+    def start_instance_checker(self) -> None:
+        thread = threading.Thread(target=self.instance_checker_target_fun)
+        thread.start()
+
+    def prune(self) -> None:
+        command = ["docker", "system", "prune", "-f"]
+        try:
+            subprocess.check_call(command)
+        except subprocess.CalledProcessError:
+            pass
+
